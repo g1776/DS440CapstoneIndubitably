a627e4dc6f79af8d1145ede54927538f51ec3f4f
g1776 <gregoryg323@gmail.com>
2023-02-22 12:17:36 -0500

    work on demo

--- app/feature_engineering.py
+++ app/feature_engineering.py
@@ -4,6 +4,7 @@ This is so we can generate features for the app without having to run the entire
 """
 
 import pandas as pd
+import numpy as np
 import pickle
 from typing import List
 import re
@@ -12,6 +13,7 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import RegexpTokenizer
 from gensim.models.word2vec import Word2Vec
+from sklearn.manifold import TSNE
 
 
 def remove_stopwords_and_lemmatize(tokens) -> list:
@@ -163,9 +165,29 @@ class DemoClassifier:
                 }
             )
 
+        # figure out which t-SNE features there are in the feature set
+        self.__tsnes_to_generate = []
+        tsnes = [feature for feature in self.__features if feature.startswith("tsne")]
+        for tsne_feature in tsnes:
+            n_dims = int(tsne_feature[4])
+            col = tsne_feature.split("_")[1]
+            self.__tsnes_to_generate.append(
+                {
+                    "feature": tsne_feature,
+                    "n_dims": n_dims,
+                    "col": col,
+                }
+            )
+
         print("Init complete.")
 
+    def __generate_embedding(self, text, w2vmodel):
+        # average the word embeddings in the text
+        return np.mean([w2vmodel.wv[word] for word in text if word in w2vmodel.wv], axis=0)
+
     def predict(self, description, amenities, review):
+        features = {}
+
         # preprocess the description
         description = preprocess_text(description)
         # preprocess the review
@@ -173,13 +195,50 @@ class DemoClassifier:
         # clean amenities
         amenities = clean_amenities(amenities)
 
-        # use w2v 
+        # generate tsne features
+        for tsne_feature in self.__tsnes_to_generate:
+            if tsne_feature["col"] == "description":
+                text = description
+                w2v = self.__w2v_descriptions
+            elif tsne_feature["col"] == "comments":
+                text = review
+                w2v = self.__w2v_comments
 
-        pass
+            embeddings = self.__generate_embedding(text, w2v)
 
 
-DemoClassifier(
+            # TODO: Fix TSNE, and make sure that there is a feature generated for each dimension
+
+            # # apply TSNE with random_state=0
+            # tsne = TSNE(n_components=tsne_feature["n_dims"], random_state=0)
+            # tsne_embedding = tsne.fit_transform(embeddings.reshape(-1, 1))
+
+            # # add the features to the feature set
+            # features[tsne_feature["feature"]] = tsne_embedding
+
+        # generate ngram features
+        for ngram_feature in self.__grams_to_find:
+            features[ngram_feature["feature"]] = ngram_feature["gram"] in description
+
+        # generate amenity features
+        for amenity_feature in self.__amenities_to_find:
+            in_amenities = amenity_feature["amenity"] in amenities
+            in_review = amenity_feature["amenity"] in review
+            features[amenity_feature["feature"]] = in_amenities and in_review
+
+        features = pd.DataFrame([features], index=[0])
+
+        print(features)
+
+
+d_clf = DemoClassifier(
     r"C:\Users\grego\Documents\GitHub\DS440CapstoneIndubitably\models\best_clf_texas_florida.pickle",
     r"C:\Users\grego\Documents\GitHub\DS440CapstoneIndubitably\models\w2vmodel_comments_texas_florida.model",
     r"C:\Users\grego\Documents\GitHub\DS440CapstoneIndubitably\models\w2vmodel_description_texas_florida.model",
+)
+
+d_clf.predict(
+    "This is a description.",
+    ["Pool", "Kitchen", "Air conditioning"],
+    "This is a review.",
 )
